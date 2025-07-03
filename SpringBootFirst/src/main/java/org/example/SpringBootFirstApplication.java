153bf5de138d850264f46b7ad06fb94b0110fe9e
Rohith-Nelson <155154758+Rohith-Nelson@users.noreply.github.com>
2025-07-03 11:42:23 +0530

    Add files via upload

---
+++ SpringBootFirst/src/main/java/org/example/SpringBootFirstApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFirstApplication.class, args);
+    }
+}
